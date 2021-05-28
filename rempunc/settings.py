f1833b64fdf9a563a48c6464be90ef4120d14457
RitikKumar15 <hl411566@gmail.com>
2021-05-28 23:34:06 +0530

    last commit

--- rempunc/settings.py
+++ rempunc/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,7 +79,7 @@ WSGI_APPLICATION = 'rempunc.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -121,8 +122,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = 'static'
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
- ]
 
